64fff7ff25921a7ae4e9ea3832d9d6cb2d096f14
maikeruzu <54655520+maikeruzu@users.noreply.github.com>
2023-10-14 03:31:29 -0700

    created dummy buttons
    todo: work on buttons to look for images.
    line 12 onpress (Button.js) breaks the whole code and i'm too sleepy to figure it out.
    
    Hopefully one that is done, I can work with the API calls and stuff

--- clarifAIclone/App.js
+++ clarifAIclone/App.js
@@ -1,38 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
 //logo
 const background = require('./assets/clarifaiLogo.png');
 
 
 export default function App() {
+  
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+
+
   return (
     <View style={styles.container}>
+
       <View style={styles.imgcontainer}>
-        <Image source={background} style={styles.img} />
+        <ImageViewer 
+        placeholderImageSource={background} 
+        selectedImage={selectedImage} />
+      </View>
+
+      <View style={styles.butContainer}>
+      <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
+
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 
   imgcontainer: {
     flex: 1,
-    paddingTop: 50,
+    paddingTop: 40,
   },
 
-  img: {
-    width: 'auto',
-    height: 500,
-    borderRadius: 20,
+  butContainer: {
+    flex: 1 / 2.5,
+    alignItems: 'center',
   },
+
+  
 });
---
+++ clarifAIclone/components/Button.js
@@ -0,0 +1,52 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+
+export default function Button({ label, theme }) {
+
+    if (theme === "primary") {
+        return (
+          <View
+          style={[styles.buttonContainer]}
+          >
+            <Pressable
+              style={[styles.button, { backgroundColor: "#004eec" }]}
+              onPress={() => alert('You pressed a button.')}
+>
+              <Text style={[styles.buttonLabel, { color: "white" }]}>{label}</Text>
+            </Pressable>
+        </View>
+        );
+      }
+
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#004eec',
+  },
+  buttonLabel: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
